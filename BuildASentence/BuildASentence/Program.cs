4c0cab07be29e25879fc91ccc61e09f3d854d82d
pkshvm <mak_314@mail.ru>
2021-07-27 23:44:20 +0300

    BuildASentence finish

--- BuildASentence/BuildASentence/Program.cs
+++ BuildASentence/BuildASentence/Program.cs
@@ -1,52 +1,57 @@
-﻿//  BuildASentence  - данная  программа  конструирует 
-// предложение  путем конкатенации  пользовательского  ввода 
-//  до  тех  пор,  пока  пользователь  не  введет  команду 
-// завершения . Эта  программа  демонстрирует  использование 
-//  проверки  равенства  строк 
-using  System; 
+﻿// BuildASentence - данная программа конструирует 
+// предложение путем конкатенации пользовательского ввода 
+// до тех пор, пока пользователь не введет команду 
+// завершения . Эта программа демонстрирует использование 
+// проверки равенства строк 
+using System;
 namespace BuildASentence
-{ }
+{
     public class Program
     {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Каждая введённая вами строка ",
-            "будет добавляться в предложение, ",
-            "пока ва не введете EXIT или QUIT");
-        // Запрашиваем  пользовательский  ввод  и  соединяем 
-        // вводимые  пользователем  фразы  в  единое  целое,  пока 
-        // не  будет  введена  команда  завершения  работы 
-        string sentence = "";
-        for (;;)
+        public static void Main(string[] args)
         {
-            //  Получение  очередной  строки 
-            Console.WriteLine("Bвeдитe  строку");
-            string line = Console.ReadLine();
-            //  Выход  при  вводе  команды  завершения 
-            string[] terms = { "EXIT", "exit", "QUIT", "quit" };
-            //  Срuниваем  введенную  строку  с  командами  вывода
-            bool quitting = false;
-            foreach (string term in terms)
+            Console.WriteLine("Каждая введённая вами строка " +
+              "будет добавляться в предложение, " +
+              "пока вы не введете EXIT или QUIT");
+            // Запрашиваем пользовательский ввод и соединяем
+            // вводимые пользователем фразы в единое целое, пока
+            // не будет введена команда завершения работы
+            string sentence = "";
+            for (; ; )
             {
-                //  Прекращение  цикла  при  совпадении 
-                if (String.Compare(line, term) == 0)
+                // Получение очередной строки 
+                Console.WriteLine("Bвeдитe строку");
+                string line = Console.ReadLine();
+                // Выход при вводе команды завершения
+
+                string[] terms = { "EXIT", "exit", "QUIT", "quit" };
+                // Сравниваем введенную строку с командами вывода
+                bool quitting = false;
+                foreach (string term in terms)
                 {
-                    quitting = true;
+                    // Прекращение цикла при совпадении
+                    if (String.Compare(line, term) == 0)
+                    {
+                        quitting = true;
+                    }
                 }
-                if (quitting == true)
-                {
-                    break;
-                }
-                // В противном случае добавление введенного к строке
-                sentence = String.Concat(sentence, line);
-                // Обратная связь 
-                Console.WriteLine(" \nBы  ввели :  " + sentence);
+                    if (quitting == true)
+                    {
+                        break;
+                    }
+
+                    // В противном случае добавление введенного к строке
+                    sentence = String.Concat(sentence, line);
+                    // Обратная связь 
+                    Console.WriteLine("\nBы ввели: " + sentence);
+                
             }
-            Console.WriteLine("\nПолучилось:\n" + sentence);
-            // Ожидаем подтверждения пользователя
-            Console.WriteLine("Haжмитe <Enter> для " +
-            "завершения программы...");
-            Console.Read();
+                Console.WriteLine("\nПолучилось:\n" + sentence);
+                // Ожидаем подтверждения пользователя
+                Console.WriteLine("Haжмитe <Enter> для " +
+                "завершения программы...");
+                Console.Read();
+            
         }
     }
-    }
+}
