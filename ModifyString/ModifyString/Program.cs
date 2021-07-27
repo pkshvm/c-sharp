0149ec777a2be92c46f377e2b5ff3b3058602ea5
pkshvm <mak_314@mail.ru>
2021-07-27 21:07:03 +0300

    1

---
+++ ModifyString/ModifyString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ModifyString
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Создаём объек Student
+            Student s1 = new Student();
+            s1.Name = "Jenny";
+            // Создаём новый объект с тем же именем
+            Student s2 = new Student();
+            s2.Name = s1.Name;
+            // "Изменение"  имени  объекта  sl  не  изменяет  сам 
+            // объект, поскольку ToUpper() возвращает новую
+            // строку, не влияя на оригинал s2 
+            s2.Name = s1.Name.ToUpper();
+            Console.WriteLine("s1 - " + s1.Name +
+                                ", s2 - " + s2.Name);
+
+            // Ожидаем подтверждения пользователя Console
+            Console.WriteLine("нажмите <Enter> для " +
+                                "завершения програмы...");
+            Console.Read();
+        }
+    }
+    //   Student  - простейший класс, содержащий  строку
+    class Student
+    {
+        public String Name;
+    }
+
+}
---
+++ Program1/stroki.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Привет, " + name);
+
+            //Ожидание реакции пользователя
+            Console.WriteLine("нажмите <ENTER> для выхода......");
+            Console.Read();
+        }
+    }
+}
