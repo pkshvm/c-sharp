698a5310f4df5c8dce377c59128c24e76d01fb8c
pkshvm <mak_314@mail.ru>
2021-09-05 17:40:34 +0300

    Add RemoveWhiteSpace Program

---
+++ RemoveWhiteSpace/RemoveWhiteSpace/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+// RemoveWhiteSpace - удаление символов из предопределенного 
+// множества из заданной строки. 
+namespace RemoveWhiteSpace
+{
+    public class Program
+    { public static void Main(string[] args)
+        {
+            // Определение множества пробельных символов
+            char[] whiteSpace = {' ', '\n', '\t' };
+            // Начинаем работу со строкой, в которой имеются
+            // пробельные символы
+            string s = "  this is a\nstring";
+            Console.WriteLine("Дo:" + s);
+            // Вьrеодим строку с удаленными пробель н ыми символами
+            Console.Write("После:");
+            // Поиск пробельных символов
+            for (; ; )
+            {
+                // Ищем позиции искомых символов; если таковых в
+                // строке больше нет, выходим из цикла
+                int offset = s.IndexOfAny(whiteSpace);
+
+                if (offset == -1)
+                {
+                    break;
+                }
+                // Разбиваем строку на, две части - до найденного
+                // символа и поспе него.
+                string Ьefore = s.Substring(0, offset);
+                string after = s.Substring(offset + 1);
+                // Объединяем эти части, но уже без найденного символа.
+                s = String.Concat(Ьefore, after);
+                // Циклически ищеы спедущий пробельный символ
+                // в модифицировакиой строке s
+            }
+                Console.WriteLine(s);
+                // Ожидаем подтверждения пользователя
+                Console.WriteLine("Haжмитe <Enter> для " +
+               " завершения программы . . . ");
+                Console.Read();
+        }
+    }
+}
